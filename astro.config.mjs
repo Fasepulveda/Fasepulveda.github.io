36ada67e83d2ab144a759857d11544281eac199f
Fernando Sepulveda <fasepulveda@miners.utep.edu>
2026-07-25 16:26:43 -0400

    Configure custom domain fernandosepulveda.me

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Fasepulveda.github.io',
+  site: 'https://fernandosepulveda.me',
   vite: {
     plugins: [tailwindcss()]
   }
